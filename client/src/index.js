6267f250d323fd135cac2768d6417c642dfea011
Tong Tang <pichip22@hotmail.com>
2023-09-26 11:54:13 -0400

    set up Home.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,10 @@ import './index.css';
 import App from './components/App';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
 
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
