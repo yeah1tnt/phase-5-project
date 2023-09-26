fc628593c28289855fb223507ec55d88df7a91ee
Tong Tang <pichip22@hotmail.com>
2023-09-26 13:33:33 -0400

    added Homepage

--- README.md
+++ README.md
@@ -17,4 +17,12 @@ pipenv install flask flask-sqlalchemy flask-migrate sqlalchemy-serializer flask-
 npx create-react-app- client --use-npm
 ```
 for client folder, create react environment. PORT=4000 for scripts start.
+
+3 - Setting up users login first, ```pipenv install flask_bcrypt``` to encrypt password. Set up models for Users and initialized the database.
+```
+flask db init
+flask db revision --autogenerate -m"message"
+flask db upgrade
+```
+
 </details>
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import '../App.css';
+
+
 import Home from './Home';
 
 function App() {
-  return (
+    return (
+        <Router>
+        <div className="App">
+            <nav>
+                <Link to="/">Home</Link>
+            
+            </nav>
 
-    <Home></Home>
-  )
+            <Routes>
+            <Route exact path="/" element={<Home />} />
+            </Routes>
+
+        </div>
+        </Router>
+    )
   
 }
 
